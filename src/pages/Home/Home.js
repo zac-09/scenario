2d019863a7374e18ce169f8f0fde6010098eb969
Fortune-Muhumuza <fortunemuhumuza@gmail.com>
2024-01-27 16:14:08 +0300

    (feat): add dynamic figure display to dashboard

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,8 +3,26 @@ import styles from "./home.module.css";
 
 import InfoTile from "../../components/InfoTile/InfoTile";
 import Sidebar from "../../components/Sidebar/Sidebar";
+import { useEffect, useState } from "react";
 
 const Home = (props) => {
+  const [randomDigits, setRandomDigits] = useState({ totalScenarios: 0, testedScenarios: 0, completedScenarios: 0 });
+
+  useEffect(() => {
+    let randomDigitsInterval;
+    randomDigitsInterval = setInterval(() => {
+      setRandomDigits((prev) => ({
+        ...prev,
+        totalScenarios: prev.totalScenarios + Math.ceil(Math.random() * 10),
+        testedScenarios: prev.testedScenarios + Math.ceil(Math.random() * 5),
+        completedScenarios: prev.completedScenarios + Math.ceil(Math.random() * 2)
+      }));
+    }, 2000);
+
+    // Clear the interval when the component is unmounted
+    return () => clearInterval(randomDigitsInterval);
+  }, []);
+
   return (
     <div className={styles["container"]}>
       <div className={styles["content"]}>
@@ -12,19 +30,19 @@ const Home = (props) => {
           <InfoTile
             style={styles["tile-blue"]}
             text="total scenarios"
-            figure="20"
+            figure={randomDigits.totalScenarios}
             icon="puzzle-piece"
           />
           <InfoTile
             style={styles["tile-teal"]}
             text="tested scenarios"
-            figure="15"
+            figure={randomDigits.testedScenarios}
             icon="lab"
           />
           <InfoTile
             style={styles["tile-pink"]}
             text="completed scenarios"
-            figure="5"
+            figure={randomDigits.completedScenarios}
             icon="checkmark2"
           />
         </div>
